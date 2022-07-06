0294c52ac1d79fcaa26491f44ff2085755141b4c
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-07-06 14:36:31 +0300

    images added... Flex.styled component added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,29 @@
-import { StyledHeader } from "./styles/Header.styled";
+import { StyledHeader, Nav, Logo, Image } from "./styles/Header.styled";
+import { Container } from "./styles/Container.styled";
+import { Button } from "./styles/Button.styled";
+import { Flex } from "./styles/Flex.styles";
 
 export default function Header() {
   return (
-    <StyledHeader bg = 'red'>
-        <h1>Hubble</h1>
+    <StyledHeader>
+      <Container>
+        <Nav>
+          <Logo src="./images/logo.svg" alt="" />
+          <Button>Try It Free</Button>
+        </Nav>
+        <Flex>
+          <div>
+            <h1>Build The Community Your Fans Will Love</h1>
+            <p>
+              Huddle re-imagines the way we build communities. You have a voice,
+              but so does your audience. Create connections with your users as
+              you engage in genuine discussion.
+            </p>
+            <Button bg='#f09'>Get Started For Free</Button>
+          </div>
+          <Image src='./images/illustration-mockups.svg'/>
+        </Flex>
+      </Container>
     </StyledHeader>
-  )
+  );
 }
